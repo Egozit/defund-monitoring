aaf3d6747bbf55af076ebfee1f9e36bb400a1daa
Egor Petrov <iampetroved@gmail.com>
2022-06-11 15:32:26 +0300

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,4 +1,4 @@
-#Archway monitoring variables template 
-COS_BIN_NAME=insert_path_to_archway_binary # example: /root/go/bin/archwayd or /home/user/go/bin/archwayd
+#DeFund monitoring variables template 
+COS_BIN_NAME=insert_path_to_archway_binary # example: /root/go/bin/defundd or /home/user/go/bin/defundd
 COS_PORT_RPC=insert_port_here              # default: 26657
-COS_VALOPER=insert_valoper_address         # example: archwayvaloper1234545636767376535673
+COS_VALOPER=insert_valoper_address         # example: defundvaloper1234545636767376535673
