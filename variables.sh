645fe3a39e6a0db69cb514915afad1981297a203
Egor Petrov <iampetroved@gmail.com>
2022-06-11 15:29:49 +0300

    Add files via upload

---
+++ variables.sh
@@ -0,0 +1,4 @@
+#Archway monitoring variables template 
+COS_BIN_NAME=insert_path_to_archway_binary # example: /root/go/bin/archwayd or /home/user/go/bin/archwayd
+COS_PORT_RPC=insert_port_here              # default: 26657
+COS_VALOPER=insert_valoper_address         # example: archwayvaloper1234545636767376535673
